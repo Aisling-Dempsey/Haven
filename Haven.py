a0af94b5856e5899a00a151c82acd2e024e2722a
Aisling Dempsey <aisling.n.dempsey@gmail.com>
2016-05-03 22:51:43 -0700

    built app/db connection, functioning debug toolbar

--- Haven.py
+++ Haven.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+from models import connect_to_db
+import os
+
 
 app = Flask(__name__)
 
+# makes jinja throw error if requesting undefined value
+app.jinja_env.undefined = StrictUndefined
+
+# gets secret key from secrets.sh
+SECRET_KEY = os.environ['HAVEN_SECRET_KEY']
+app.secret_key= SECRET_KEY
 
 @app.route('/')
 def hello_world():
@@ -9,4 +20,14 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    # toggles debug mode on
+    app.debug = True
+
+    # runs debug toolbar
+    DebugToolbarExtension(app)
+
+    # connects to database
+    connect_to_db(app)
+
+    # runs app
     app.run()
