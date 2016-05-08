14fa145cd4206cab6d35f6667ee384ce96fa6aad
Aisling Dempsey <aisling.n.dempsey@gmail.com>
2016-05-07 21:59:50 -0700

    rough initial main models and routes added. server renamed.

--- models.py
+++ models.py
@@ -6,9 +6,42 @@ db = SQLAlchemy()
 ##############################################################################
 # DB Models
 
+class User(db.model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(15), nullable=False)
 
 
+class Business(db.model):
+    __tablename__ = "businesses"
 
+    business_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    # TODO determine if db.string needs a max length
+    address = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    # TODO determine if db.Integer can have max length
+    zipcode = db.Column(db.Integer(5), nullable=False)
+    phone = db.Column(db.Integer())
+    # TODO lat/long nullable?
+    lat_long = db.Column([db.Integer, db.Integer])
+
+
+class Rating(db.model):
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    business_id = db.Column(db.Integer, db.ForeignKey('businesses.business_id'))
+    score = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(500))
+
+
+    user = db.relationship("User", backref=db.backref("ratings", order_by=rating_id))
+    business = db.relationship("Business", backref=db.backref("ratings", order_by=rating_id))
 
 ##############################################################################
 # Helper functions
---
+++ server.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+from models import connect_to_db
+from yelpapi import YelpAPI
+
+import os
+
+
+app = Flask(__name__)
+
+# makes jinja throw error if requesting undefined value
+app.jinja_env.undefined = StrictUndefined
+
+# gets secret key from secrets.sh
+
+
+SECRET_KEY = os.environ['HAVEN_SECRET_KEY']
+app.secret_key = SECRET_KEY
+
+
+# dict of api keys to be passed into every render template for google maps api
+KEYS = {'maps': os.environ['MAPS_SECRET_KEY']
+        }
+
+YELP_CONSUMER_KEY = os.environ['YELP_CONSUMER_KEY']
+YELP_CONSUMER_SECRET = os.environ['YELP_CONSUMER_SECRET']
+YELP_TOKEN = os.environ['YELP_TOKEN']
+YELP_TOKEN_SECRET = os.environ['YELP_TOKEN_SECRET']
+
+
+# instantiates yelp api with appropriate keys.
+yelp_api = YelpAPI(YELP_CONSUMER_KEY, YELP_CONSUMER_SECRET, YELP_TOKEN, YELP_TOKEN_SECRET)
+
+
+# EXAMPLE YELP QUERIES
+# search_results = yelp_api.search_query(args)
+# business_results = yelp_api.business_query(id=business_id, other_args)
+# phone_search_results = yelp_api.phone_search_query(phone=phone_number, other_args)
+
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/')
+def splash():
+    """splash page"""
+    return render_template('splash.html', keys=KEYS)
+
+
+@app.route('/login')
+def login():
+    """Login page"""
+    return render_template('construction.html', keys=KEYS)
+
+
+@app.route('/results')
+def results():
+    """search results page"""
+    return render_template('construction.html', keys=KEYS)
+
+
+@app.route('/explore')
+def explore():
+    """explore page"""
+    return render_template('construction.html', keys=KEYS)
+
+@app.route('/:username')
+def user_account(username):
+    """User account page"""
+    return render_template('construction.html', keys=KEYS)
+
+@app.route('/:username/favorites')
+def favorites(username):
+    """user favorites"""
+    return render_template('construction.html', keys=KEYS)
+
+@app.route('/:username/ratings')
+def ratings(username):
+    """user ratings"""
+    return render_template('construction.html', keys=KEYS)
+
+@app.route('/:username/manage')
+def account_manage(username)
+    """allows update of user information. Requires new login"""
+    return render_template('construction.html', keys=KEYS)
+
+if __name__ == '__main__':
+    # toggles debug mode on
+    app.debug = True
+
+    # runs debug toolbar
+    DebugToolbarExtension(app)
+
+    # connects to database
+    connect_to_db(app)
+
+    # runs app
+    # app.run()
